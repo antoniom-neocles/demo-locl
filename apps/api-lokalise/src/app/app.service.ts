55452f629bd45d74fb40a0d1abf3302e3a6c0200
Antonio Martino <antonio.martino@neocles.io>
2020-03-11 11:50:06 +0100

    Filtered out translations based on verified and translated criteria and added new translations in both the modules in order to test how lokalise and angular 9 manage the duplications.

--- apps/api-lokalise/src/app/app.service.ts
+++ apps/api-lokalise/src/app/app.service.ts
@@ -5,50 +5,55 @@ import * as request from 'request-promise-native';
 
 @Injectable()
 export class AppService {
-
   private apiKey = '460899465e65fb3562ea14.10852720';
   private projectID = 'ac4dcd625dd3baf902632f9b2192e0227f1041b2';
   private api: LokaliseAPI;
 
-  constructor () {
+  constructor() {
     this.api = new LokaliseAPI({
       token: this.projectID
     });
   }
 
   public async downloadFiles(language: string, filename: string) {
-    const apiResponse = await this.api.files.download(
-      this.apiKey,
-      {
+    try {
+      const apiResponse = await this.api.files.download(this.apiKey, {
         format: 'json',
         original_filenames: true,
         filter_filenames: [`${filename}.%LANG_ISO%.xlf`],
-        filter_langs: [language]
-      }
-    );
-    const zipFile = await request({
-      method: 'GET',
-      url: apiResponse.bundle_url,
-      encoding: null
-    });
-
-    const zip = await JSZip.loadAsync(zipFile);
-
-    return zip
-      .file(`${language}/${filename}.${language}.json`)
-      .async('string')
-      .then(data => {
-        // remove the following line of code when you'll figure out 
-        // the issue which causes the wrong interpretation of the generic 
-        // translations tokens from xlf1.2 to json performed by lokalise api.
-        const newData = data.replace(/<x id=\\"([a-zA-Z0-9_-]*)\\"\\\/>/g, `{$)$1}`).replace(/\$\)/g, '$');
-
-        return {locale: language, translations: JSON.parse(newData)};
+        filter_langs: [language],
+        filter_data: ['nonfuzzy', 'translated']
       });
+      const zipFile = await request({
+        method: 'GET',
+        url: apiResponse.bundle_url,
+        encoding: null
+      });
+
+      console.log(apiResponse.bundle_url);
+  
+      const zip = await JSZip.loadAsync(zipFile);
+  
+      return zip
+        .file(`${language}/${filename}.${language}.json`)
+        .async('string')
+        .then(data => {
+          // Remove the following line of code after received help form the lokalise devOps
+          // about an issue in the files download apis which causes a misinterpretation of the generic
+          // translations tokens from xlf1.2 if you download converting to json.
+          const newData = data
+            .replace(/<x id=\\"([a-zA-Z0-9_-]*)\\"\\\/>/g, `{$)$1}`)
+            .replace(/\$\)/g, '$');
+  
+          return { locale: language, translations: JSON.parse(newData) };
+        });
+    } catch (error) {
+      return Promise.resolve({ locale: language, translations: {} });
+    }
   }
 
   //the library that I'm using for this POC is not updated with the current version of lokalise APIv2 (which implements a filter called 'filter_lang_id'),
-  // so it doesn't support filtering for language or file name.
+  // so it doesn't support filtering for language, neither for file name.
   public async getTranslations(language: string) {
     return await this.api.translations
       .list(this.projectID)
--- apps/demo/src/app/lazy-loaded/lazy-loaded.component.ts
+++ apps/demo/src/app/lazy-loaded/lazy-loaded.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'locl-lazy-loaded',
   template: `
     <p i18n="lazy-loaded|Test string used for translating the lazy loaded module@@Lazy loading works">Lazy loading works</p>
+    <h1 i18n="lazy-loaded|Label for testing@@label testing">Label for testing the translations</h1>
   `,
   styles: []
 })
